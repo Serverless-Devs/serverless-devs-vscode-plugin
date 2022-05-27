1ae9ac93acc5b9ab651971f480d7943d08d92384
xsahxl <xsahxl@126.com>
2022-05-27 16:55:38 +0800

    feat: 🎸 terminal

--- src/commands/deploy/index.ts
+++ src/commands/deploy/index.ts
@@ -1,5 +1,17 @@
 import * as vscode from "vscode";
+import * as path from "path";
+import { ext } from "../../extensionVariables";
+import * as core from "@serverless-devs/core";
 
-export function deploy() {
+export async function deploy() {
   vscode.window.showInformationMessage("this message from deploy");
+  const spath = path.join(ext.cwd, "s.yaml");
+  const hasYaml = await core.getYamlContent(spath);
+  if (hasYaml) {
+    const terminal = vscode.window.createTerminal(
+      `deploy from Serverless Devs`
+    );
+    terminal.show();
+    terminal.sendText("s deploy");
+  }
 }
