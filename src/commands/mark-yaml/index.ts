bd949232a3298fd458981cc38db73eeb447ca4a8
xsahxl <xsahxl@126.com>
2022-06-06 14:49:54 +0800

    feat: 🎸 手动标记yaml 数据存储到.serverless-devs

--- src/commands/mark-yaml/index.ts
+++ src/commands/mark-yaml/index.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as path from "path";
 import * as core from "@serverless-devs/core";
 import * as fs from "fs";
 import { ext } from "../../extensionVariables";
@@ -15,10 +16,40 @@ export async function markYaml(uri: vscode.Uri) {
         return name.length === 0 ? "value cannot be empty." : undefined;
       },
     });
-    const yamlData = await core.getYamlContent(fsPath);
-    yamlData.alias = answer;
-    const doc = core.modifyYaml(yamlData, fs.readFileSync(fsPath, "utf-8"));
-    fs.writeFileSync(fsPath, doc, "utf-8");
+    const appPath = path.dirname(fsPath);
+    const filePath = path.join(appPath, ".serverless-devs");
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, JSON.stringify({}));
+    }
+    const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    if (!Array.isArray(data["vscode-marked-yamls"])) {
+      data["vscode-marked-yamls"] = [
+        {
+          path: fsPath,
+          alias: answer,
+        },
+      ];
+    } else {
+      const findObj = data["vscode-marked-yamls"].find(
+        (item) => item.path === fsPath
+      );
+      if (findObj) {
+        data["vscode-marked-yamls"] = data["vscode-marked-yamls"].map(
+          (item) => {
+            if (item.path === fsPath) {
+              item.alias = answer;
+            }
+            return item;
+          }
+        );
+      } else {
+        data["vscode-marked-yamls"].push({
+          path: fsPath,
+          alias: answer,
+        });
+      }
+    }
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
     ext.localResource.refresh();
   } catch (error) {
     vscode.window.showErrorMessage("yaml文件不符合Serverless Devs规范");
