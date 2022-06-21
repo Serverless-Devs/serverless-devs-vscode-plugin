a62addb3e1de9d573cd7ce72c5083e2cc9e65e47
0xff01 <898731820@qq.com>
2022-06-22 03:20:07 +0800

    feat: 🎸 增加密钥完善

--- src/add-key/ui/main.js
+++ src/add-key/ui/main.js
@@ -1,32 +1,75 @@
+import * as core from "@serverless-devs/core";
+
 const vscode = acquireVsCodeApi();
 
 new Vue({
   el: "#app",
   data: {
+    pickProvider: '',
     providerItems: [],
     accessList: [],
-    pickProvider: ''
+    pickProviderKeys: [],
+    normalKeyValue: [],
+    customKeyValue: [
+      {
+        id: 1,
+        kvPair: ''
+      }
+    ],
+    num: 1
   },
   created() {
     this._getProviderItems();
-    this._getaccessList();
+    this._getAccessList();
   },
   methods: {
     _getProviderItems() {
       this.providerItems = this.$config.items;
     },
-    _getaccessList() {
+    _getAccessList() {
       this.accessList = this.$config.configAccessList;
     },
     getProviderPickValue() {
       this.$nextTick(() => {
-        this.pickProvider = _.get(this.accessList, this.$refs.providerDropdown.currentValue);
-        
+        this.pickProvider = this.$refs.providerDropdown.currentValue;
+        this.pickProviderKeys = _.get(this.accessList, this.$refs.providerDropdown.currentValue);
       });
+      this.normalKeyValue = [];
     },
+    addKvPair() {
+      this.customKeyValue.push({id: this.num++, kvPair: ''});
+    },
+    subKvPair(index) {
+      this.customKeyValue.splice(index, 1);
+    },
+    normalKV(key, event) {
+      this.normalKeyValue[key] = event.currentTarget.value;
+      console.log( this.normalKeyValue);
+    },
+    customKV(index, event) {
+      this.customKeyValue[index].kvPair = event.currentTarget.value;
+    },
+    async submitCredential() {
+      if ( this.pickProvider === "alibaba") {
+        try {
+          const data = await core.getAccountId(this.normalKeyValue);
+          this.normalKeyValue.AccountID = data.AccountId;
+          const {...rest } = this.normalKeyValue;
+          await core.setKnownCredential(rest, this.pickProvider);
+        } catch (error) {
+          console.log(1111);
+          return;
+        }
+      } else {
+        const {...rest } = this.pickProvider === "custom" ? 
+          this.customKeyValue : this.normalKeyValue;
+        await core.setKnownCredential(rest, this.pickProvider);
+      }
+      console.log(`Add ${this.pickProvider} configuration successfully.`)
+    }
   },
   watch: {
-    pickProvider: function(newVal, oldVal) {
+    customKeyValue: function(newVal, oldVal) {
       console.log(newVal);
     }
   }
